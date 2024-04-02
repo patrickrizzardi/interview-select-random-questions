fb2936425ec4cb93596ee9a9a9b347279d5f9756
Patrick <35274364+RedactDigital@users.noreply.github.com>
2024-04-02 17:12:29 -0400

    Clearify instructions

--- examples/exampleOne.js
+++ examples/exampleOne.js
@@ -1,23 +1,23 @@
 import questions from '../questions';
 
 /**
- * Given an array of question objects
- * Question object:
- *   {
- *     difficulty: string,
- *     instructions: string,
- *     question: string,
- *     options: string[],
- *     correctAnswer: string,
- *   }
- *
- * Create a function that returns an array of the following:
- * 3 questions where difficulty = 'advanced'
- * 5 questions where difficulty = 'intermediate'
- * 12 questions where difficulty = 'beginner'
- *
- * The returned array should have a total of 20 questions at the end.
- */
+* Create a function that generates an array of 20 unique random questions from the questions array in the './questions.js' file, categorized by difficulty levels.
+*
+* @param {Object[]} questions - An array of question objects.
+* Each object has the following structure:
+*   {
+*     difficulty: string, // Level of question difficulty ('advanced', 'intermediate', 'beginner')
+*     instructions: string, // Instructions for the question
+*     question: string, // The question text
+*     options: string[], // Array of possible answers
+*     correctAnswer: string, // Correct answer
+*   }
+*
+* @returns {Object[]} - An array of 20 unique random questions, distributed as follows:
+* - 3 questions of 'advanced' difficulty
+* - 5 questions of 'intermediate' difficulty
+* - 12 questions of 'beginner' difficulty
+*/
 
 const createQuestions = questions => {
   const totalAdvancedQuestions = 3;
